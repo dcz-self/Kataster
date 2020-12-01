1984ec9dc49d4b46821e25415645e4df55f165d1
dcz <gihuac.dcz@porcupinefactory.org>
2020-12-01 15:23:12 +0000

    Reformat contact system

--- src/contact.rs
+++ src/contact.rs
@@ -34,111 +34,113 @@ pub fn contact_system(
     mut mobs: Query<Mut<Mob>>,
     handles: Query<&RigidBodyHandleComponent>,
 ) {
-    if runstate.gamestate.is(GameState::Arena) {
-        let mut contacts = vec![];
-        while let Ok(contact_event) = events.contact_events.pop() {
-            match contact_event {
-                ContactEvent::Started(h1, h2) => {
-                    let e1 = *(bh_to_e.0.get(&h1).unwrap());
-                    let e2 = *(bh_to_e.0.get(&h2).unwrap());
-                    if ships.get_mut(e1).is_ok() && damages.get(e2).is_ok() {
-                        contacts.push(Contacts::ShipAsteroid(e1, e2));
-                    } else if ships.get_mut(e2).is_ok() && damages.get(e1).is_ok() {
-                        contacts.push(Contacts::ShipAsteroid(e2, e1));
-                    }
-                }
-                _ => (),
-            };
-        }
-        while let Ok(proximity_event) = events.proximity_events.pop() {
-            let mut asteroids = &mut mobs;
-            if proximity_event.new_status == Proximity::Intersecting {
-                let e1 = *(bh_to_e.0.get(&proximity_event.collider1).unwrap());
-                let e2 = *(bh_to_e.0.get(&proximity_event.collider2).unwrap());
-                if asteroids.get_mut(e2).is_ok() && lasers.get_mut(e1).is_ok() {
-                    contacts.push(Contacts::LaserAsteroid(e1, e2));
-                } else if asteroids.get_mut(e1).is_ok() && lasers.get_mut(e2).is_ok() {
-                    contacts.push(Contacts::LaserAsteroid(e2, e1));
+    if !runstate.gamestate.current().is_arena() {
+        return;
+    }
+     
+    let mut contacts = vec![];
+    while let Ok(contact_event) = events.contact_events.pop() {
+        match contact_event {
+            ContactEvent::Started(h1, h2) => {
+                let e1 = *(bh_to_e.0.get(&h1).unwrap());
+                let e2 = *(bh_to_e.0.get(&h2).unwrap());
+                if ships.get_mut(e1).is_ok() && damages.get(e2).is_ok() {
+                    contacts.push(Contacts::ShipAsteroid(e1, e2));
+                } else if ships.get_mut(e2).is_ok() && damages.get(e1).is_ok() {
+                    contacts.push(Contacts::ShipAsteroid(e2, e1));
                 }
             }
+            _ => (),
+        };
+    }
+    while let Ok(proximity_event) = events.proximity_events.pop() {
+        let mut asteroids = &mut mobs;
+        if proximity_event.new_status == Proximity::Intersecting {
+            let e1 = *(bh_to_e.0.get(&proximity_event.collider1).unwrap());
+            let e2 = *(bh_to_e.0.get(&proximity_event.collider2).unwrap());
+            if asteroids.get_mut(e2).is_ok() && lasers.get_mut(e1).is_ok() {
+                contacts.push(Contacts::LaserAsteroid(e1, e2));
+            } else if asteroids.get_mut(e1).is_ok() && lasers.get_mut(e2).is_ok() {
+                contacts.push(Contacts::LaserAsteroid(e2, e1));
+            }
         }
-        for contact in contacts.into_iter() {
-            match contact {
-                Contacts::LaserAsteroid(e1, e2) => {
-                    let mut asteroids = &mut mobs;
-                    let laser_handle = handles
-                        .get(e1)
-                        .unwrap()
-                        .handle();
-                    let asteroid = asteroids.get_mut(e2).unwrap();
-                    let asteroid_handle = handles
-                        .get(e2)
-                        .unwrap()
-                        .handle();
-                    runstate.score = runstate.score.and_then(|score| {
-                        Some(
-                            score
-                                + match asteroid.size {
-                                    AsteroidSize::Small => 40,
-                                    AsteroidSize::Medium => 20,
-                                    AsteroidSize::Big => 10,
-                                },
-                        )
-                    });
-                    {
-                        let laser_body = bodies.get(laser_handle).unwrap();
-                        let asteroid_body = bodies.get(asteroid_handle).unwrap();
+    }
+    for contact in contacts.into_iter() {
+        match contact {
+            Contacts::LaserAsteroid(e1, e2) => {
+                let mut asteroids = &mut mobs;
+                let laser_handle = handles
+                    .get(e1)
+                    .unwrap()
+                    .handle();
+                let asteroid = asteroids.get_mut(e2).unwrap();
+                let asteroid_handle = handles
+                    .get(e2)
+                    .unwrap()
+                    .handle();
+                runstate.score = runstate.score.and_then(|score| {
+                    Some(
+                        score
+                            + match asteroid.size {
+                                AsteroidSize::Small => 40,
+                                AsteroidSize::Medium => 20,
+                                AsteroidSize::Big => 10,
+                            },
+                    )
+                });
+                {
+                    let laser_body = bodies.get(laser_handle).unwrap();
+                    let asteroid_body = bodies.get(asteroid_handle).unwrap();
 
-                        explosion_spawn_events.send(ExplosionSpawnEvent {
-                            kind: ExplosionKind::LaserOnAsteroid,
-                            x: laser_body.position.translation.x,
-                            y: laser_body.position.translation.y,
-                        });
-                    }
-                    commands.despawn(e1);
-                    commands.despawn(e2);
+                    explosion_spawn_events.send(ExplosionSpawnEvent {
+                        kind: ExplosionKind::LaserOnAsteroid,
+                        x: laser_body.position.translation.x,
+                        y: laser_body.position.translation.y,
+                    });
                 }
-                Contacts::ShipAsteroid(e1, e2) => {
-                    let player_body = bodies
-                        .get(
-                            handles
-                                .get(e1)
-                                .unwrap()
-                                .handle(),
-                        )
-                        .unwrap();
-                    let mut borg = ships.get_mut(e1).unwrap();
-                    let damage = damages.get(e2).unwrap();
-                    borg.life = borg.life.saturating_sub(damage.value);
-                    if borg.life <= 0 {
-                        explosion_spawn_events.send(ExplosionSpawnEvent {
-                            kind: ExplosionKind::ShipDead,
-                            x: player_body.position.translation.x,
-                            y: player_body.position.translation.y,
-                        });
-                        commands.despawn_recursive(e1);
-                        // FIXME: despawn LookAts
-                        // This is kind of flaky... There could be a separate system to catch brainful despawns.
-                        let score = runstate.score.unwrap_or(0);
-                        match genotypes.get(e1) {
-                            Ok(genotype) => runstate.shooter_gene_pool.preserve(
-                                genotype.clone(),
-                                score as f64,
-                            ),
-                            Err(QueryError::NoSuchEntity) => {},
-                            Err(e) => println!("Borg unuseable genotype {:?}", e),
-                        }
-                    } else {
-                        explosion_spawn_events.send(ExplosionSpawnEvent {
-                            kind: ExplosionKind::ShipContact,
-                            x: player_body.position.translation.x,
-                            y: player_body.position.translation.y,
-                        });
+                commands.despawn(e1);
+                commands.despawn(e2);
+            }
+            Contacts::ShipAsteroid(e1, e2) => {
+                let player_body = bodies
+                    .get(
+                        handles
+                            .get(e1)
+                            .unwrap()
+                            .handle(),
+                    )
+                    .unwrap();
+                let mut borg = ships.get_mut(e1).unwrap();
+                let damage = damages.get(e2).unwrap();
+                borg.life = borg.life.saturating_sub(damage.value);
+                if borg.life <= 0 {
+                    explosion_spawn_events.send(ExplosionSpawnEvent {
+                        kind: ExplosionKind::ShipDead,
+                        x: player_body.position.translation.x,
+                        y: player_body.position.translation.y,
+                    });
+                    commands.despawn_recursive(e1);
+                    // FIXME: despawn LookAts
+                    // This is kind of flaky... There could be a separate system to catch brainful despawns.
+                    let score = runstate.score.unwrap_or(0);
+                    match genotypes.get(e1) {
+                        Ok(genotype) => runstate.shooter_gene_pool.preserve(
+                            genotype.clone(),
+                            score as f64,
+                        ),
+                        Err(QueryError::NoSuchEntity) => {},
+                        Err(e) => println!("Borg unuseable genotype {:?}", e),
                     }
-                    let mob = mobs.get_mut(e2).unwrap();
-                    runstate.mob_gene_pool.preserve(mob.genotype().clone());
-                    commands.despawn(e2);
+                } else {
+                    explosion_spawn_events.send(ExplosionSpawnEvent {
+                        kind: ExplosionKind::ShipContact,
+                        x: player_body.position.translation.x,
+                        y: player_body.position.translation.y,
+                    });
                 }
+                let mob = mobs.get_mut(e2).unwrap();
+                runstate.mob_gene_pool.preserve(mob.genotype().clone());
+                commands.despawn(e2);
             }
         }
     }
