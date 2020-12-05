08d901f6fdb5dae3d598de28aa7811ba09da760a
dcz <gihuac.dcz@porcupinefactory.org>
2020-12-05 14:07:26 +0000

    Meshmaker with colors

--- tools/meshmaker/src/main.rs
+++ tools/meshmaker/src/main.rs
@@ -6,7 +6,7 @@
  */
 
 use immense as im;
-use immense::{ cube, write_meshes, ExportConfig, OutputMesh };
+use immense::{ cube, write_meshes, ExportConfig, OutputMesh, Replicate, Rule, Tf };
 use std::fs::File;
 use std::io;
 
@@ -17,7 +17,17 @@ enum Error {
 }
 
 fn double_helix() -> impl Iterator<Item=OutputMesh> {
-    let rule = cube();
+    let rule = Rule::new()
+        .push(vec![Tf::saturation(0.3), Tf::hue(160.0), Tf::tx(3.0)], cube())
+        .push(vec![Tf::saturation(0.3), Tf::tx(-3.0)], cube());
+    let rule = Rule::new()
+        .push(
+            Replicate::n(
+                100,
+                vec![Tf::ry(36.0), Tf::ty(2.0)],
+            ),
+            rule,
+        );
     rule.generate()
 }
 
@@ -25,8 +35,14 @@ fn write_to_file(meshes: impl Iterator<Item=OutputMesh>, name: &str)
     -> Result<(), Error>
 {
     let mut output_file = File::create(name).map_err(Error::Io)?;
-    write_meshes(ExportConfig::default(), meshes, &mut output_file)
-        .map_err(Error::Immense)?;
+    write_meshes(
+        ExportConfig {
+            export_colors: Some(format!("{}.mtl", name)),
+            ..ExportConfig::default()
+        },
+        meshes,
+        &mut output_file,
+    ).map_err(Error::Immense)?;
     Ok(())
 }
 
